13b201810caa0bb8e91c2569c4782ac67eb4950f
RedCat771 <savelyev7979@gmail.com>
2020-11-13 23:14:30 +0200

    осталось сделать футер

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,34 @@
-$(".slider").slick({
-  dots: true,
-  prevArrow:
-    '<button type="button" class="slick-prev"><img src="../images/src/slide-prev.svg" alt="#"></button>',
-  nextArrow:
-    '<button type="button" class="slick-next"><img src="../images/src/slide-next.svg" alt="#"></button>',
+$(function () {
+  $(".slider").slick({
+    dots: true,
+    prevArrow:
+      '<button type="button" class="slick-prev"><img src="../images/src/slide-prev.svg" alt="#"></button>',
+    nextArrow:
+      '<button type="button" class="slick-next"><img src="../images/src/slide-next.svg" alt="#"></button>',
+  });
+
+  $(".gallery__slider").slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    centerMode: true,
+    variableWidth: true,
+    prevArrow:
+      '<button type="button" class="slick-prev"><img src="../images/src/slide-prev.svg" alt="#"></button>',
+    nextArrow:
+      '<button type="button" class="slick-next"><img src="../images/src/slide-next.svg" alt="#"></button>',
+  });
+
+  $(".partners__slider").slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 5,
+    variableWidth: true,
+    prevArrow:
+      '<button type="button" class="slick-prev"><img src="../images/src/slide-prev.svg" alt="#"></button>',
+    nextArrow:
+      '<button type="button" class="slick-next"><img src="../images/src/slide-next.svg" alt="#"></button>',
+  });
 });
