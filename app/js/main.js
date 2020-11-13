e4a784b1b9ef8517121e1e4e033b1871277ce934
RedCat771 <savelyev7979@gmail.com>
2020-11-13 14:30:04 +0200

    start

---
+++ app/js/main.js
@@ -0,0 +1,7 @@
+$(".slider").slick({
+  dots: true,
+  prevArrow:
+    '<button type="button" class="slick-prev"><img src="../images/src/slide-prev.svg" alt="#"></button>',
+  nextArrow:
+    '<button type="button" class="slick-next"><img src="../images/src/slide-next.svg" alt="#"></button>',
+});
