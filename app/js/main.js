da8872c10942f630c5f5c8cf09ffba84949aaac1
RedCat771 <savelyev7979@gmail.com>
2020-11-14 11:09:47 +0200

    finish desktop version

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,17 @@
 $(function () {
+  $(".menu a, .footer__link").on("click", function (event) {
+    //отменяем стандартную обработку нажатия по ссылке
+    event.preventDefault();
+
+    //забираем идентификатор бока с атрибута href
+    var id = $(this).attr("href"),
+      //узнаем высоту от начала страницы до блока на который ссылается якорь
+      top = $(id).offset().top;
+
+    //анимируем переход на расстояние - top за 1500 мс
+    $("body,html").animate({ scrollTop: top }, 1500);
+  });
+
   $(".slider").slick({
     dots: true,
     prevArrow:
